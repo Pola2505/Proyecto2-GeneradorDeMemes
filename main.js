c2ce82dd6b158afcd38dfce2e76628b42e4c6bef
Sandra <sandra27popielarczyk@gmail.com>
2024-12-15 19:03:16 -0300

    agregada la funcion de cargar la imagen del meme

--- main.js
+++ main.js
@@ -9,7 +9,9 @@ const $textBtn = $('#text-btn')
 const $aside = $('#edit-container-aside');
 const $editImg = $('#edit-img');
 const $editText = $('#edit-text');
-
+const $closeBtn = $('#close-btn');
+const $urlInput = $('#url-input');
+const $imgCard = $('#img-card');
 
 
 // Abrir las opciones de edicion de imagen
@@ -27,6 +29,25 @@ $textBtn.addEventListener('click', () => {
     $editText.classList.remove('hidden');
     $editImg.classList.add('hidden');
 })
+
+// La funcion de cerrar el panel de control - por las dudas lo aplique para todos los 3 elementos
+
+// $closeBtn.addEventListener('click', () => {
+//     $aside.classList.add('hidden');
+//     $editText.classList.add('hidden');
+//     $editImg.classList.add('hidden');
+// })
+
+// La funcion para pegar la url de la imagen y que aparezca en el cuadrado del meme
+
+$urlInput.addEventListener('input', () => {
+    const url = $urlInput.value;
+    $imgCard.style.backgroundImage = `url(${url})`;
+    $imgCard.style.backgroundSize = 'cover';
+    $imgCard.style.backgroundRepeat = 'no-repeat';
+    $imgCard.backgroundPosition = 'center';
+})
+
 
 
 
