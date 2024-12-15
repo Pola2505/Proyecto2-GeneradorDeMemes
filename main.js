8ba5818a04bd346047a0ea9c5ef3b3968368e754
Sandra <sandra27popielarczyk@gmail.com>
2024-12-15 17:37:10 -0300

    la funcion de abrir la opcion de imagen agregada

--- main.js
+++ main.js
@@ -1,13 +1,40 @@
-$botonDescargar.addEventListener("click", () => {
-    // $card.style.width = "500px"
-    domtoimage.toBlob($card).then((blob) => {
-        saveAs(blob, "mi-meme.png");
-    }).catch((error) =>{
-        console.log (error)
-    } ) ;
+// La funcion para atrapar los elementos del DOM
 
+const $ = (element) => document.querySelector(element);
+
+// Atrapando los elementos del DOM 
+
+const $imgBtn = $('#img-btn');
+const $aside = $('#edit-container-aside');
+const $editImg = $('#edit-img');
+
+
+
+
+// Funcion para abrir la seccion de Imagen 
+
+$imgBtn.addEventListener('click', () => {
+    $aside.classList.remove('hidden');
+    $editImg.classList.remove('hidden');
 })
 
-$cargarImagen.addEventListener("click", () => {
-    $contenedorImagen.style.backgroundImage = `url(${$urlImagen.value})`
-});
+
+
+
+
+
+
+
+// $botonDescargar.addEventListener("click", () => {
+//     // $card.style.width = "500px"
+//     domtoimage.toBlob($card).then((blob) => {
+//         saveAs(blob, "mi-meme.png");
+//     }).catch((error) =>{
+//         console.log (error)
+//     } ) ;
+
+// })
+
+// $cargarImagen.addEventListener("click", () => {
+//     $contenedorImagen.style.backgroundImage = `url(${$urlImagen.value})`
+// });
